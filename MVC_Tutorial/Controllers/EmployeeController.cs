768b5a4223c4d42caacd85f71365465d3e245f88
kanangandhi93 <kanangandhi93@gmail.com>
2016-12-17 16:45:21 +0530

    Model Tutorial 1

---
+++ MVC_Tutorial/Controllers/EmployeeController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using MVC_Tutorial.Models;
+
+namespace MVC_Tutorial.Controllers
+{
+    public class EmployeeController : Controller
+    {
+        // GET: Employee
+        public ActionResult Index()
+        {
+            Employee emp = new Models.Employee()
+            {
+                Id = 1,
+                Name = "Kanan"
+            };
+
+            return View(emp);
+        }
+    }
+}
---
+++ MVC_Tutorial/Models/Employee.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MVC_Tutorial.Models
+{
+    public class Employee
+    {
+        public Int64 Id { get; set; }
+        public String Name { get; set; }
+    }
+}
